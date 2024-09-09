6f2b2beac9a0dc92ac8f5fb70540d002045b12ab
Sem1Rose <AppdullahMohammed@gmail.com>
2024-09-09 17:47:44 +0300

    Finished basic world generation.

--- Storm/Assets/Scripts/World Generation/WorldGenerator.cs
+++ Storm/Assets/Scripts/World Generation/WorldGenerator.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using UnityEditor.EditorTools;
 using UnityEngine;
 using UnityEngine.Tilemaps;
@@ -7,7 +8,7 @@ public class WorldGenerator : MonoBehaviour
     [System.Serializable]
     public struct Tile_ {
         // public Tile tile;
-        public TileBase tile;
+        public Tile tile;
         [Range(0,1)]
         public float depth;
     }
@@ -21,17 +22,20 @@ public class WorldGenerator : MonoBehaviour
     [Space]
     [Header("Noise Variables")]
     public int seed;
+    [Tooltip("Number of layered noise functions")]
     [Range(1, 10)]
     public int octaves = 3;
     public float amplitude = 10;
-    public float frequency = 1;
+    [Tooltip("How zoomed out (< 10) or zoomed in (> 10) the first noise layer is")]
     public float scale = 1;
+    [Tooltip("How much to zoom out each of the octaves")]
+    public float frequency = 1;
+    [Tooltip("How much influence each octave has -> higher = more fine details")]
     [Range(0, 1)]
     public float lacunarity = 1;
     public Vector2Int offset = Vector2Int.zero;
 
-
-    [HideInInspector]
+    // [HideInInspector]
     public int xPos;
 
     void Start()
@@ -40,14 +44,17 @@ public class WorldGenerator : MonoBehaviour
         // seed = (int)Random.Range(-0xFFFFFFFF, 0xFFFFFFFF);
         Random.InitState(seed);
         xPos = 0;
+
+        // BuildWorld();
     }
 
     void Update()
     {
         tilemap.ClearAllTiles();
+        transform.position = new Vector3(xPos, transform.position.y, transform.position.z);
+        // temporary to visualize changes
         BuildWorld();
     }
-
 
     void BuildWorld()
     {
@@ -77,10 +84,35 @@ public class WorldGenerator : MonoBehaviour
     {
         for (int x = -chunkSize; x < chunkSize; x++)
         {
-            float noiseVal = GetNoiseVal(x);
+            int noiseVal = Mathf.RoundToInt(GetNoiseVal(x));
 
-            tilemap.SetTile(new Vector3Int(x, Mathf.RoundToInt(noiseVal)), tiles[0].tile);
+            tilemap.SetTile(new Vector3Int(x, noiseVal), tiles[0].tile);
+            FillColumn(x, noiseVal);
+
             tilemap.SetTile(new Vector3Int(x, chunkBottomLimit), tiles[^1].tile);
+        }
+    }
+
+    void FillColumn(int x, int y)
+    {
+        Tile_[] _tiles = tiles.ToList().OrderBy(x => x.depth).Reverse().ToArray();
+
+        for (int i = chunkBottomLimit + 1; i < y; i++)
+        {
+            float depth = Remap(i, chunkBottomLimit, y, 0, 1);
+
+            TileBase availableTile = tiles.ToList().FirstOrDefault(x => x.depth < depth).tile;
+            // TileBase[] availableTiles = tiles.ToList().FindAll(x => x.depth < depth).Select(x => x.tile).ToArray();
+
+
+            // if (availableTiles.Length == 0)
+            // {
+            //     Debug.LogError("There probably isn't a tile with a depth of 0!", this);
+            //     continue;
+            // }
+
+            // int randomTile = Random.Range(0, availableTiles.Length - 1);
+            tilemap.SetTile(new Vector3Int(x, i), availableTile);
         }
     }
 
@@ -89,7 +121,7 @@ public class WorldGenerator : MonoBehaviour
         float noiseVal = 0, scl = scale / 10, amp = amplitude;
         for (int i = 1; i <= octaves; i++)
         {
-            float rawVal = Mathf.Clamp01(Mathf.PerlinNoise((x + offset.x + 0xFFFF) * scl, seed)) * amp + offset.y;
+            float rawVal = Mathf.Clamp01(Mathf.PerlinNoise((x + offset.x + xPos + 0xFFFF) * scl, seed)) * amp + offset.y;
 
             scl *= frequency;
             amp *= lacunarity;
@@ -99,4 +131,6 @@ public class WorldGenerator : MonoBehaviour
 
         return noiseVal;
     }
+
+    float Remap(float val, float fromMin, float fromMax, float toMin, float toMax) => toMin + (val-fromMin)*(toMax-toMin)/(fromMax-fromMin);
 }
